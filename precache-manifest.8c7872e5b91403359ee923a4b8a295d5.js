506c899b7a59254aa6178a94a14b4768cd109c11
Maryem Souayah <maryemsouayah3@gmail.com>
2020-04-20 02:16:50 +0100

    Updates

---
+++ precache-manifest.8c7872e5b91403359ee923a4b8a295d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9048405f8212223904178c27b617cdd3",
+    "url": "/ReactTP_useState/index.html"
+  },
+  {
+    "revision": "9b1139bf23aa7c674d5d",
+    "url": "/ReactTP_useState/static/css/main.11d10b71.chunk.css"
+  },
+  {
+    "revision": "d3b73e96f42460ef46a9",
+    "url": "/ReactTP_useState/static/js/2.59cc14fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactTP_useState/static/js/2.59cc14fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1139bf23aa7c674d5d",
+    "url": "/ReactTP_useState/static/js/main.4cee7bb1.chunk.js"
+  },
+  {
+    "revision": "ddd4356b35fe9b01e2c8",
+    "url": "/ReactTP_useState/static/js/runtime-main.e55e6428.js"
+  }
+]);
